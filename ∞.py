c5bf43e77b7fb9078f86f68732f26c1f0d20b652
Kaushik Dev <64822905+g00fy57@users.noreply.github.com>
2021-07-14 05:14:39 +0530

    Add files via upload

---
+++ ∞.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"eyyowhatifitoldyouthatmylifeisinfinite"
+
+def euler_illumination():
+ #euler
+"e"=2.718
+"pi"=3.14
+"i"=root -1   
+Omega= (e^pi.i+1)
+Alpha= (∞)
+Omega = Alpha
+
+euler_illumination
